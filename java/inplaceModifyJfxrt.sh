fb46cfcb5cceb33cb33737380e95b018e6ac8998
Chandler Griscom <cjgriscom@gmail.com>
2019-09-23 10:42:25 -0400

    Fix bindings memory leak

--- java/inplaceModifyJfxrt.sh
+++ java/inplaceModifyJfxrt.sh
@@ -4,6 +4,22 @@ rm -R bin || true
 mkdir bin
 
 unzip /tmp/jfxrt.jar -d bin/
+javac -cp bin/ com/sun/javafx/binding/BindingHelperObserver.java
+javac -cp bin/ com/sun/javafx/binding/ExpressionHelperBase.java
+javac -cp bin/ javafx/beans/property/BooleanPropertyBase.java
+javac -cp bin/ javafx/beans/property/DoublePropertyBase.java
+javac -cp bin/ javafx/beans/property/FloatPropertyBase.java
+javac -cp bin/ javafx/beans/property/IntegerPropertyBase.java
+javac -cp bin/ javafx/beans/property/ListPropertyBase.java
+javac -cp bin/ javafx/beans/property/LongPropertyBase.java
+javac -cp bin/ javafx/beans/property/MapPropertyBase.java
+javac -cp bin/ javafx/beans/property/ObjectPropertyBase.java
+javac -cp bin/ javafx/beans/property/SetPropertyBase.java
+javac -cp bin/ javafx/beans/property/StringPropertyBase.java
+
+jar -uf /tmp/jfxrt.jar com/sun/javafx/binding/*.class
+jar -uf /tmp/jfxrt.jar javafx/beans/property/*.class
+
 javac -cp bin/ com/sun/glass/ui/monocle/EGL.java
 jar -uf /tmp/jfxrt.jar com/sun/glass/ui/monocle/EGL.class
 cp com/sun/glass/ui/monocle/EGL.class bin/com/sun/glass/ui/monocle/EGL.class
