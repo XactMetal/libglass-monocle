a6f1e51c264bcd0ea0b410e67f6aa81884d9d088
Chandler Griscom <chandler@xactmetal.com>
2021-01-25 21:21:50 +0000

    Remove fullBuild.sh

--- fullBuild.sh
+++
@@ -1,8 +0,0 @@
-set -e
-make
-pushd java
-./inplaceModifyJfxrt.sh
-popd
-sudo cp libglass_monocle.so /usr/lib/jvm/java-8-oracle/jre/lib/arm/libglass_monocle.so
-sudo cp /tmp/jfxrt.jar /usr/lib/jvm/java-8-oracle/jre/lib/ext/jfxrt.jar
-echo Done all
